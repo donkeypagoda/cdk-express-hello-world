bac9d2d00793be59c4df24471f1ff400d37ddd46
donkeypagoda <christopherallenstewart@gmail.com>
2023-06-27 12:36:17 -0700

    Initial commit

---
+++ test/cdk-express-hello-world.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkExpressHelloWorld from '../lib/cdk-express-hello-world-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-express-hello-world-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkExpressHelloWorld.CdkExpressHelloWorldStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
